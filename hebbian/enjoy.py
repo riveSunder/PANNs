25c85e65ccbd22c81d7fe6ae250f403c8fcff5fb
rlygr <rivesunder@protonmail.com>
2020-04-22 15:36:39 -0600

    update evaluatin/visualization script

--- hebbian/enjoy.py
+++ hebbian/enjoy.py
@@ -37,7 +37,7 @@ if __name__ =="__main__":
             help="name of environment",\
             default="LunarLanderContinuous-v2")
     parser.add_argument("-m", "--model_filepath", nargs="+",type=str,\
-            help="model to load", default=["./models/exp559445/model_epann2_clamp10_sd_43110_gen99.h5"])
+            help="model to load", default=["./models/exp105724/model_epann2_clamp100_sd_1337_gen99.h5"])
     parser.add_argument("-s", "--steps", type=int,\
             help="number of steps in gravity range", default = 7)
     parser.add_argument("-x", "--max_gravity_x", type=float,\
@@ -69,27 +69,33 @@ if __name__ =="__main__":
     xx = 0
     yy = 0
     
+    plt.figure()
     for model_filepath in model_filepaths:
         
         agent.load_state_dict(torch.load(model_filepath))
 
         xx = 0
-
-        for gravity_x in np.linspace(- args.max_gravity_x, args.max_gravity_x, args.steps):
+        fitnesses = []
+        trial = 0
+        for gravity_x in np.linspace(- args.max_gravity_x, args.max_gravity_x * (1+1/steps)\
+                , args.steps):
             yy = 0
-            for gravity_y in np.linspace(-args.max_gravity_y, args.max_gravity_y, args.steps):
+            for gravity_y in np.linspace(-args.max_gravity_y, args.max_gravity_y*(1+1/args.steps),\
+                    args.steps):
 
                 env.env.world.gravity = b2Vec2(\
                         g_x + gravity_x,\
-                        - g_y + gravity_y)
+                        g_y + gravity_y)
 
 
                 fitness = []
                 total_steps = 0
+                agent.reset_hebbians()
                 for epd in range(epds):
                     obs = env.reset()
                     done = False
                     accumulated_reward = 0
+                    steps = 0 
                     while not done:
                         action = agent.forward(\
                             torch.Tensor(obs).reshape(1,obs.shape[0]))
@@ -99,23 +105,26 @@ if __name__ =="__main__":
                             action = action.squeeze()
 
                         obs, rew, done, info = env.step(action.detach().numpy())
-                        if 1:
+                        if 0:
                             env.render()
                             time.sleep(0.01)
+                        
 
+                        steps += 1
                         total_steps += 1
                         accumulated_reward += rew
                     fitness.append(accumulated_reward)
 
-
+                fitnesses.append(fitness)
+                trial += 1
                 print(xx,yy)
                 res_mat[xx,yy] = np.mean(fitness)
                 print("gravity x: {:.2e} y: {:.2e} mean fitness {:.2e}".format(\
                         gravity_x, gravity_y, res_mat[xx,yy]))
                 print(agent.W, agent.dopa)
                 #print(fitness)
-
-                print_stats_fit(0, fitness, total_steps, gravity_x, -4.9 + gravity_y, clamp=None)
+                plt.plot(fitness)
+                print_stats_fit(trial, fitness, total_steps, gravity_x, g_y + gravity_y, clamp=None)
 
                 yy += 1
             xx += 1
