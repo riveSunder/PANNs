816c350de66eb439670df8aa3ca098011ad2427c
rlygr <rivesunder@protonmail.com>
2020-04-22 15:37:05 -0600

    update experimental definition for exp_01

--- exp_fickle_gravity_01.sh
+++ exp_fickle_gravity_01.sh
@@ -4,11 +4,8 @@ source ~/.venv/epanns/bin/activate
 
 cd ~/Desktop/earl/hebbian
 
-python3 hebbian/train.py -w 43 -p 92 -e 8 -g 100 -n LunarLanderContinuous-v2 -c 0.1 0.01 0.0 1.0 -s 43770 
-python3 hebbian/train.py -w 43 -p 92 -e 8 -g 100 -n LunarLanderContinuous-v2 -c 0.0 0.01 0.1 1.0 -s 43110
-python3 hebbian/train.py -w 43 -p 92 -e 8 -g 100 -n LunarLanderContinuous-v2 -c 0.0 0.01 0.1 1.0 -s 13371355
-
-python3 hebbian/train.py -w 43 -p 92 -e 8 -g 100 -n LunarLanderContinuous-v2 -c 0.0 0.01 0.1 1.0 -s 15 
-python3 hebbian/train.py -w 43 -p 92 -e 8 -g 100 -n LunarLanderContinuous-v2 -c 0.0 0.01 0.1 1.0 -s 18
-
-python3 hebbian/train.py -w 43 -p 92 -e 8 -g 100 -n LunarLanderContinuous-v2 -c 0.0 0.01 0.1 1.0 -s 116
+python3 hebbian/train.py -w 46 -p 92 -e 4 -g 200 -n LunarLanderContinuous-v2 -c 0.0 0.01 0.1 1.0 -s 13 
+python3 hebbian/train.py -w 46 -p 92 -e 4 -g 200 -n LunarLanderContinuous-v2 -c 0.0 0.01 0.1 1.0 -s 42
+python3 hebbian/train.py -w 46 -p 92 -e 4 -g 200 -n LunarLanderContinuous-v2 -c 0.0 0.01 0.1 1.0 -s 501337
+python3 hebbian/train.py -w 46 -p 92 -e 4 -g 200 -n LunarLanderContinuous-v2 -c 0.0 0.01 0.1 1.0 -s 139 
+python3 hebbian/train.py -w 46 -p 92 -e 4 -g 200 -n LunarLanderContinuous-v2 -c 0.0 0.01 0.1 1.0 -s 43110
